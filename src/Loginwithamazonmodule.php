9a0d157d3ba09d3bda7fac8386d0c1e41aadecf8
xyz <114230687+hackerhero0x01@users.noreply.github.com>
2026-01-05 19:03:55 +0530

    Create Loginwithamazonmodule.php

---
+++ src/Loginwithamazonmodule.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Amzn\\Login-with-amazon-module;
+
+class Loginwithamazonmodule
+{
+    public static function hello()
+    {
+        return "Hello from amzn/login-with-amazon-module";
+    }
+}
